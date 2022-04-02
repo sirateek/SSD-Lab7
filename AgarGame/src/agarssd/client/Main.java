a1b09cf032366a8dc0e6e0e1ee770834def7c840
Siratee K <sirateek@gmail.com>
2022-04-02 15:32:35 +0700

    Update the strategy in Main for testing

--- AgarGame/src/agarssd/client/Main.java
+++ AgarGame/src/agarssd/client/Main.java
@@ -3,6 +3,6 @@ package agarssd.client;
 public class Main {
     public static void main(String[] args) {
         GameClient gameClient = new GameClient();
-        gameClient.start(new RandomStrategy());
+        gameClient.start(new PointStragegy());
     }
 }
