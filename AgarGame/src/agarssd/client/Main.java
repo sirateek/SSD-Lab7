0f80096d1463201982d08a9d4ef32f93c4d9a53d
Siratee K <sirateek@gmail.com>
2022-04-02 14:27:45 +0700

    Add initial code for agar game

---
+++ AgarGame/src/agarssd/client/Main.java
@@ -0,0 +1,8 @@
+package agarssd.client;
+
+public class Main {
+    public static void main(String[] args) {
+        GameClient gameClient = new GameClient();
+        gameClient.start();
+    }
+}
